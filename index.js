98953b04e5f1c0f007e21a322061d00c35d1f331
Naif Adnan <146568103+adnan99999977@users.noreply.github.com>
2025-09-08 14:29:25 +0600

    some style add

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const displayCategories = (categoriesList) => {
   categoriesContainer.innerHTML = "";
     const allTreesDiv = document.createElement("p");
   allTreesDiv.className =
-    "categories_div cursor-pointer rounded-sm p-2 mb-2 text-[rgba(31,41,55,1)] text-[16px]";
+    "categories_div cursor-pointer  hover:bg-[#51c57f9c] duration-300 rounded-sm p-2 mb-2 text-[rgba(31,41,55,1)] text-[16px]";
   allTreesDiv.innerText = "All Trees";
   allTreesDiv.onclick = () => allPlantsLoad();
   categoriesContainer.appendChild(allTreesDiv);
@@ -26,7 +26,7 @@ const displayCategories = (categoriesList) => {
   categoriesList.forEach((category) => {
     const createNewCategoryList = document.createElement("div");
     createNewCategoryList.innerHTML = `
-       <div class = " cursor-pointer ">
+       <div class = " cursor-pointer  hover:bg-[#51c57f9c] duration-300 rounded-sm ">
        <p  onclick="categoryBtn(${category.id})"  class=" categories_div rounded-sm p-2 text-[rgba(31,41,55,1)] text-[16px] ">${category.category_name}</p></div>`;
     categoriesContainer.appendChild(createNewCategoryList);
   });
@@ -59,17 +59,17 @@ const displayPlantsByCatagory = (plants) => {
     const createCardByCategory = document.createElement("div");
     createCardByCategory.innerHTML = `
       <div class="plant_card lg:w-60  bg-white rounded-lg p-5 lg:p-3 space-y-2 shadow-md cursor-pointer">
-              <img class="rounded-md w-full h-[170px]
+              <img class="rounded-md w-full h-[170px] m-auto
              lg:w-[210px] lg:h-[210px] object-cover
              " src="${plant.image}" alt="">
-             <button class="plant_name font-bold cursor-pointer text-left hover:text-green-800 " onclick="showModal(${plant.id})">${plant.name}</button>
+             <button class="plant_name mt-1 font-bold cursor-pointer text-left hover:text-green-600 text-lg duration-300  " onclick="showModal(${plant.id})">${plant.name}</button>
            <div class = "h-[87px] overflow-hidden">
              <p class="text-[12px]">${plant.description}</p></div>
              <div class="price flex justify-between items-center">
                 <p class="p-2 font-bold text-[12px] bg-[rgba(220,252,231,1)] rounded-4xl text-[rgba(21,128,61,1)] ">${plant.category}</p>
                 <p class="plant_price font-bold">৳${plant.price}</p>
              </div>
-             <button  class=" add_to_cart_btn cursor-pointer bg-[rgba(21,128,61,1)] rounded-3xl w-full text-white p-2 lg:p-1 mt-2" >Add to Cart</button>
+             <button  class=" add_to_cart_btn cursor-pointer bg-[rgba(21,128,61,1)] rounded-3xl w-full text-white p-2 lg:p-1 mt-2 hover:bg-[rgba(29,181,61,1)] duration-200" >Add to Cart</button>
           </div>
       `;
      
@@ -102,10 +102,10 @@ const allPlantsDisplay = (allPlants) => {
   allPlants.forEach((plant) => {
     const createPlantsCard = document.createElement("div");
     createPlantsCard.innerHTML = ` <div class="plant_card lg:w-60  bg-white rounded-lg p-5 lg:p-3 space-y-2 shadow-md cursor-pointer">
-              <img class="rounded-md w-full h-[170px]
+              <img class="rounded-md w-full m-auto h-[170px]
              lg:w-[210px] lg:h-[210px] object-cover
              " src="${plant.image}" alt="">
-             <button class="plant_name font-bold cursor-pointer text-left hover:text-green-800 " onclick="showModal(${plant.id})">${plant.name}</button>
+             <button class="plant_name text-lg mt-1  font-bold cursor-pointer text-left hover:text-green-600 " onclick="showModal(${plant.id})">${plant.name}</button>
             <div class="h-[87px] overflow-hidden">
              <p class="text-[12px] ">${plant.description}</p></div>
              <div class="price flex justify-between items-center">
@@ -136,9 +136,9 @@ const displayTreesDetails = (plant) => {
   createInfoModal.innerHTML = `
     <h2 class="text-2xl font-bold mb-2">${plant.name}</h2>
     <img class="rounded-lg w-full h-[300px] object-cover  mb-3" src="${plant.image}" alt="${plant.name}">
-    <p><span class="font-bold">Category:</span> ${plant.category}</p>
-    <p><span class="font-bold">Price:</span> ৳${plant.price}</p>
-    <p class="mt-2 text-sm text-gray-600"><span class="font-bold">Description:</span>${plant.description}</p>
+    <p><span class="font-bold">Category: </span> ${plant.category}</p>
+    <p><span class="font-bold">Price: </span> ৳${plant.price}</p>
+    <p class="mt-2 text-sm text-gray-800 "><span class="font-bold text-lg">Description: </span>${plant.description}</p>
   `;
   modalInfoContainer.appendChild(createInfoModal);
 };
@@ -167,7 +167,7 @@ const addToCartBtn = (info) => {
 
   const createNewCart = document.createElement("div");
   createNewCart.innerHTML = `
-         <div  class="cart_card p-4 lg:p-2 rounded-sm  flex justify-between items-center mb-4 w-[200px] m-auto bg-[rgba(240,253,244,1)] text-[#474646]">
+         <div  class="cart_card p-4 lg:p-2 rounded-sm  flex justify-between items-center mb-4 w-[300px] lg:w-[200px] m-auto bg-[rgba(240,253,244,1)] text-[#474646]">
                <div class="info">
                 <p class="font-bold">${info.name}</p>
                 <p>${info.price} x 1</p>
